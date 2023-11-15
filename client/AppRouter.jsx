cb612577eb8ca7001e464cdd970d0d92a391cdcf
Kris Oud <oudster@gmail.com>
2023-11-14 21:42:39 -0500

    Added /profile
    Added the /profile route to view users account details
    
    Added link to navigation

--- client/AppRouter.jsx
+++ client/AppRouter.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Landing from "./src/core/Landing.jsx";
 import SignUp from "./src/core/SignUp.jsx";
 import SignIn from "./src/core/SignIn.jsx";
+import Profile from "./src/core/Profile.jsx";
 import GameExperience from "./src/core/GameExperience.jsx";
 
 const AppRouter = () => {
@@ -11,6 +12,7 @@ const AppRouter = () => {
         <Route exact path="/" element={<Landing />} />
         <Route exact path="/signup" element={<SignUp />} />
         <Route exact path="/signin" element={<SignIn />} />
+        <Route exact path="/profile" element={<Profile />} />
         <Route exact path="/play" element={<GameExperience />} />
       </Routes>
     </>
