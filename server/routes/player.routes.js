f3cd0ab364e4d8494fe34cd458472cda508ac424
emankh1 <ealkhat1@my.centennialcollege.ca>
2023-11-17 11:29:39 -0500

    Create player.routes.js

---
+++ server/routes/player.routes.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import playerCtrl from '../controllers/player.controller.js' 
+import authCtrl from '../controllers/auth.controller.js'
+
+const router = express.Router()
+
+router.route('/api/player') 
+    .get(playerCtrl.list)
+    .post(playerCtrl.create)
+
+router.route('/api/player/:playerById')
+    .get(authCtrl.requireSignin, playerCtrl.read)
+    .put(authCtrl.requireSignin, authCtrl.hasAuthorization, playerCtrl.update)
+    .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, playerCtrl.remove)
+
+router.param('playerId', playerCtrl.playerByID)
+
+export default router
