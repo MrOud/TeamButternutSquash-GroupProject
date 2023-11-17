aa353044be27db2228964ca87b22dc8c0ef47694
Kris Oud <oudster@gmail.com>
2023-11-17 01:46:18 -0500

    added the routes and controller for news
    Added the route and controller for news and have the news render out to the play page

--- server/models/news.model.js
+++ server/models/news.model.js
@@ -1,22 +1,23 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
+const NewsSchema = new mongoose.Schema({
+  eventDate: {
+    type: Date,
+    trim: true,
+    default: Date.now,
+  },
 
-const NewsSchema = new mangoose.Schema ( {
-    eventDate: {
-        type: Date, 
-        trim: true, 
-        default: Date.now
-    }, 
+  playerRef: {
+    type: String,
+    trim: true,
+    required: "player ID required",
+  },
 
-    playerRef: {
-        type : String, 
-        trim: true,
-        required: 'player ID required'
-    }, 
+  message: {
+    type: String,
+    trim: true,
+    required: "message required",
+  },
+});
 
-    message: {
-        type: String,
-        trim: true,
-        required: 'message required'
-    }
-})
+export default mongoose.model("News", NewsSchema);
