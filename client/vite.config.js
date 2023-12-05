912564bd85da11e96001ffcfd96540b66056b614
Kris Oud <oudster@gmail.com>
2023-12-04 21:28:58 -0500

    fix?
    please please please

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,24 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const PORT = 3000;
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-      "/auth": {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-    },
-  },
   build: {
     outDir: "../dist/app",
   },
